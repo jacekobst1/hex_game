b3e41d404788404664160d5ed08b57aa4e9fc269
Jacek Obst <j.obst@teb-akademia.pl>
2020-12-13 12:58:06 +0100

    window_size and game_board_size in constructor + make methods private

--- main_new.py
+++ main_new.py
@@ -9,27 +9,29 @@ from PySide2.QtCore import Qt, QPointF, QRect
 class MyWin(QGraphicsView):
     def __init__(self):
         super(MyWin, self).__init__()
-        self.setGeometry(1000, 1000, 1000, 1000)
-        self.setWindowTitle('Hex game')
-
+        self.window_size = 1000
+        self.game_board_size = 11
         self.scene = QGraphicsScene()
+
+        self.setGeometry(self.window_size, self.window_size, self.window_size, self.window_size)
+        self.setWindowTitle('Hex game')
         self.setScene(self.scene)
 
-        self.init_menu()
+        self.__init_menu()
 
-    def init_menu(self):
+    def __init_menu(self):
         menu_bar = QMenuBar(self)
         menu_bar.setGeometry(QRect(0, 0, 1000, 25))
 
         new_game_action = menu_bar.addAction('New game')
-        new_game_action.triggered.connect(self.restart_game)
+        new_game_action.triggered.connect(self.__restart_game)
 
-    def restart_game(self):
-        if not hasattr(self, 'brush') or self.restart_game_prompt():
-            self.init_paint_tools()
-            self.init_game_board(11)
+    def __restart_game(self):
+        if not hasattr(self, 'brush') or self.__restart_game_prompt():
+            self.__init_paint_tools()
+            self.__init_game_board()
 
-    def restart_game_prompt(self) -> bool:
+    def __restart_game_prompt(self) -> bool:
         prompt_window = QMessageBox
         choice = prompt_window.question(self,
                                         'New game',
@@ -37,20 +39,20 @@ class MyWin(QGraphicsView):
                                         prompt_window.Yes | prompt_window.No)
         return choice == prompt_window.Yes
 
-    def init_paint_tools(self):
+    def __init_paint_tools(self):
         self.brush = QBrush(QColor(255, 255, 255, 255))
         self.pen = QPen(QColor(0, 0, 0), 1, Qt.SolidLine)
 
-    def init_game_board(self, size: int):
+    def __init_game_board(self):
         hexagon_shape = QHexagonShape(0, 0, 30, 90)
         x_offset = 50
         y_offset = 45
         next_row_x_offset = 25
 
-        for row_i in range(size):
+        for row_i in range(self.game_board_size):
             current_row_x_offset = row_i * next_row_x_offset
             y = row_i * y_offset
-            for col_i in range(size):
+            for col_i in range(self.game_board_size):
                 x = col_i * x_offset + current_row_x_offset
                 self.scene.addPolygon(hexagon_shape, self.pen, self.brush).setPos(QPointF(x, y))
 
@@ -59,11 +61,11 @@ class MyWin(QGraphicsView):
         selected_tile = self.scene.itemAt(click_position, QTransform())
         self.brush.setColor(QColor(0, 0, 255, 255))
         if selected_tile is not None:
-            self.paint_graphic_item(selected_tile, brush=self.brush)
+            self.__paint_graphic_item(selected_tile, brush=self.brush)
             print(selected_tile.pos())
 
     @staticmethod
-    def paint_graphic_item(graphic_item: QGraphicsPolygonItem,
+    def __paint_graphic_item(graphic_item: QGraphicsPolygonItem,
                            pen: QPen = None,
                            brush: QBrush = None):
         if pen is not None:
