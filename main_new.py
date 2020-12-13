0f7c6cb1161eb1feffdaecbbc74892e64177353e
Jacek Obst <j.obst@teb-akademia.pl>
2020-12-13 15:40:36 +0100

    Add list of tiles - check tile owner every mouse click

--- main_new.py
+++ main_new.py
@@ -11,10 +11,11 @@ class MyWin(QGraphicsView):
         super(MyWin, self).__init__()
         self.__window_size = 1000
         self.__game_board_size = 11
-        self.__init_color = QColor(255, 255, 255, 255)              # white
-        self.__player_1 = Player(QColor(51, 153, 255, 255))         # blue
-        self.__player_2 = Player(QColor(255, 255, 51, 255))         # yellow
+        self.__init_color = QColor(255, 255, 255, 255)  # white
+        self.__player_1 = Player(QColor(51, 153, 255, 255))  # blue
+        self.__player_2 = Player(QColor(255, 255, 51, 255))  # yellow
         self.__current_player = None
+        self.__tiles = []
         self.__scene = QGraphicsScene()
 
         self.setGeometry(self.__window_size, self.__window_size, self.__window_size, self.__window_size)
@@ -59,16 +60,27 @@ class MyWin(QGraphicsView):
             y = row_i * y_offset
             for col_i in range(self.__game_board_size):
                 x = col_i * x_offset + current_row_x_offset
-                self.__scene.addPolygon(hexagon_shape, self.__pen, self.__brush).setPos(QPointF(x, y))
+                position = QPointF(x, y)
+                self.__tiles.append({
+                    'row': row_i,
+                    'col': col_i,
+                    'position': position,
+                    'owner': None
+                })
+                self.__scene.addPolygon(hexagon_shape, self.__pen, self.__brush).setPos(position)
 
     def mousePressEvent(self, event: QMouseEvent):
         click_position = self.mapToScene(event.pos())
         selected_tile = self.__scene.itemAt(click_position, QTransform())
-        self.__brush.setColor(self.__current_player.get_color())
+        self.__brush.setColor(self.__current_player.color)
+
         if selected_tile is not None:
-            self.__paint_graphic_item(selected_tile, brush=self.__brush)
-            self.change_player()
-            print(selected_tile.pos())
+            tile_from_list = list([tile for tile in self.__tiles if tile['position'] == selected_tile.pos()])[0]
+
+            if tile_from_list['owner'] is None:
+                tile_from_list['owner'] = self.__current_player
+                self.__paint_graphic_item(selected_tile, brush=self.__brush)
+                self.change_player()
 
     def change_player(self):
         if self.__current_player == self.__player_1:
@@ -91,9 +103,10 @@ class MyWin(QGraphicsView):
 
 class Player:
     def __init__(self, color: QColor):
-        self.__color = color
+        self.__color: QColor = color
 
-    def get_color(self) -> QColor:
+    @property
+    def color(self) -> QColor:
         return self.__color
 
 
