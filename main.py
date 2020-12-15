f5e1c4c61e423aa5375a802d262283e954449d34
świdur <dawid@swidurski.pl>
2020-12-15 03:16:04 +0100

    Tile is an object, calculating path uses graphs

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import os
 import sys
 
-from operator import itemgetter
+from PySide2.QtCore import Qt, QPointF, QRect
 from PySide2.QtGui import QBrush, QColor, QPen, QTransform, QMouseEvent
 from PySide2.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsPolygonItem, QMenuBar, QMessageBox
+
+from graph import Graph
 from hexagon import QHexagonShape
-from PySide2.QtCore import Qt, QPointF, QRect
 
 
 class Player:
-    def __init__(self, color: QColor, direction_type: str):
+    def __init__(self, color: QColor, direction_type: str, name: str):
+        self.name = name
         self.__color: QColor = color
         self.__direction_type: str = direction_type
+        self.tile_graph = Graph({})
 
     @property
     def color(self) -> QColor:
@@ -21,15 +25,27 @@ class Player:
         return self.__direction_type
 
 
+class Tile:
+    def __init__(self, row, col, position, owner):
+        self.row = row
+        self.col = col
+        self.position = position
+        self.owner = owner
+
+    def __str__(self):
+        return f"|{self.row}, {self.col} - {self.owner.name or 'None'}|"
+
+
 class MyWin(QGraphicsView):
     def __init__(self):
         super(MyWin, self).__init__()
+
         self.__window_size = 1000
         self.__game_board_size = 11
         self.__init_color = QColor(255, 255, 255, 255)  # white
-        self.__player_1 = Player(QColor(51, 153, 255, 255), 'row')  # blue
-        self.__player_2 = Player(QColor(255, 255, 51, 255), 'col')  # yellow
-        self.__current_player = None
+        self.__player_1 = Player(QColor(51, 153, 255, 255), 'row', 'Jim')  # blue
+        self.__player_2 = Player(QColor(255, 255, 51, 255), 'col', 'Bones')  # yellow
+        self.__current_player: Player = None
         self.__tiles = []
         self.__possible_neighbours_positions = [
             {'row_offset': -1, 'col_offset': 0},
@@ -41,7 +57,7 @@ class MyWin(QGraphicsView):
         ]
         self.__scene = QGraphicsScene()
 
-        self.setGeometry(self.__window_size, self.__window_size, self.__window_size, self.__window_size)
+        self.setGeometry(0, 0, self.__window_size, self.__window_size)
         self.setWindowTitle('Hex game')
         self.setScene(self.__scene)
 
@@ -60,6 +76,7 @@ class MyWin(QGraphicsView):
             self.__init_paint_tools()
             self.__init_game_board()
             self.__current_player = self.__player_1
+            self.__current_player.tile_graph = Graph({})
 
     def __restart_game_prompt(self, text: str) -> bool:
         prompt_window = QMessageBox
@@ -85,12 +102,7 @@ class MyWin(QGraphicsView):
             for col_i in range(self.__game_board_size):
                 x = col_i * x_offset + current_row_x_offset
                 position = QPointF(x, y)
-                self.__tiles.append({
-                    'row': row_i,
-                    'col': col_i,
-                    'position': position,
-                    'owner': None
-                })
+                self.__tiles.append(Tile(row_i, col_i, position, None))
                 self.__scene.addPolygon(hexagon_shape, self.__pen, self.__brush).setPos(position)
 
     def mousePressEvent(self, event: QMouseEvent):
@@ -99,44 +111,55 @@ class MyWin(QGraphicsView):
         self.__brush.setColor(self.__current_player.color)
 
         if selected_tile is not None:
-            tile_from_list = list([tile for tile in self.__tiles if tile['position'] == selected_tile.pos()])[0]
+            tile_from_list = list([tile for tile in self.__tiles if tile.position == selected_tile.pos()])[0]
 
-            if tile_from_list['owner'] is None:
-                tile_from_list['owner'] = self.__current_player
+            if tile_from_list.owner is None:
+                tile_from_list.owner = self.__current_player
                 self.__paint_graphic_item(selected_tile, brush=self.__brush)
-                self.__compute_result(self.__current_player)
+                self.__handle_graph(tile_from_list)
+                self.__check_path()
                 self.__change_player()
+
+    def __handle_graph(self, tile):
+        self.__current_player.tile_graph.add_vertex(tile)
+
+        friendly_neighbours = []
+
+        for possible_position in self.__possible_neighbours_positions:
+            [friendly_neighbours.append(t) for t in self.__tiles
+             if t.col == tile.col + possible_position['col_offset'] and
+             t.row == tile.row + possible_position['row_offset'] and
+             t.owner == self.__current_player]
+
+        for fn in friendly_neighbours:
+            self.__current_player.tile_graph.add_edge({fn, tile})
+
+    def __check_path(self):
+        player_tiles = list([item for item in self.__tiles if item.owner == self.__current_player])
+
+        if self.__current_player.direction_type == 'col':
+            first_dir_tiles = list([item for item in player_tiles if item.col == 0])
+            last_dir_tiles = list([item for item in player_tiles if item.col == self.__game_board_size - 1])
+        else:
+            first_dir_tiles = list([item for item in player_tiles if item.row == 0])
+            last_dir_tiles = list([item for item in player_tiles if item.row == self.__game_board_size - 1])
+
+        if len(first_dir_tiles) == 0 or len(last_dir_tiles) == 0:
+            return None
+        else:
+            print(self.__current_player.tile_graph)
+            for ft in first_dir_tiles:
+                for lt in last_dir_tiles:
+                    paths = self.__current_player.tile_graph.find_all_paths(ft, lt)
+                    if len(paths) > 0:
+                        self.__restart_game(f'{self.__current_player.name} wins! Do you want to play again?')
+                        break
 
     def __change_player(self):
         if self.__current_player == self.__player_1:
             self.__current_player = self.__player_2
         else:
             self.__current_player = self.__player_1
-
-    def __compute_result(self, player: Player):
-        player_tiles = list([item for item in self.__tiles if item['owner'] == player])
-        first_dir_tiles = list([item for item in player_tiles if item[player.direction_type] == 0])
-
-        for first_dir_tile in first_dir_tiles:
-            possible_tiles = [item for item in player_tiles if item != first_dir_tile]
-            route = [first_dir_tile]
-            self.__check_neighbours(route, first_dir_tile, possible_tiles)
-
-    def __check_neighbours(self,
-                           route: list,
-                           tile: dict,
-                           possible_tiles: list):
-        route.append(tile)
-        possible_tiles = [item for item in possible_tiles if item != tile]
-
-        for possible_neighbour_position in self.__possible_neighbours_positions:
-            for neighbour_tile in [item for item in possible_tiles
-                                        if item['row'] == tile['row'] + possible_neighbour_position['row_offset']
-                                        and item['col'] == tile['col'] + possible_neighbour_position['col_offset']]:
-                if list([item for item in route if item['row'] == self.__game_board_size - 1]):
-                    self.__restart_game('You win! Do you want to play again?')
-                    break
-                self.__check_neighbours(route, neighbour_tile, possible_tiles)
 
     @staticmethod
     def __paint_graphic_item(graphic_item: QGraphicsPolygonItem,
@@ -149,6 +172,10 @@ class MyWin(QGraphicsView):
             graphic_item.setBrush(brush)
 
         graphic_item.update()
+
+    @staticmethod
+    def cls():
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 if __name__ == "__main__":
